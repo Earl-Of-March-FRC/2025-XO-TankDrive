78bc51ae272bee312aa5c159bd628ad2a2e8b062
EomLions <eomlions@gmail.com>
2025-05-21 17:08:56 -0400

    Increase factors
    Co-authored-by: Sam Liu <sam@samliu.dev>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,8 +33,8 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger 
-    double speedFactor = 0.4;
-    double rotFactor = 0.4;
+    double speedFactor = 0.5;
+    double rotFactor = 0.5;
     double deadband = 0.05;
     drivetrainSubsystem.setDefaultCommand(new DriveArcadeCMD(
       drivetrainSubsystem,
